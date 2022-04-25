3503a08be587f4c4e08b23fccfac5c9fd39abb1c
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-04-25 16:18:54 +0200

    ajout liste déroulante ligue

--- src/components/FormReservation/FormResa.js
+++ src/components/FormReservation/FormResa.js
@@ -1,33 +1,46 @@
-import React from 'react';
 import './FormResa.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHotel } from '@fortawesome/free-solid-svg-icons';
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import React, {useRef} from 'react';
+import axios from 'axios';
 
 
 
 function FormResa() {
+    const date = useRef();
+    const salle = useRef();
+    const handleResa = async (e) => {
+        e.preventDefault();
+        console.log(date);
+        console.log(date.current.value);
+        console.log(salle);
+        console.log(salle.current.value);
+    }
+    
+        
+
 
   return(
       <div>
-        <form>
+        <form onSubmit={handleResa}>
             <div class="containerFormResa">
                 <h2 class="titreForm">Reservation</h2>
                 <div>
                     <label for="dateResa" class="labelResa"></label>
-                    <input type="date" name="ResaDate" id="Resa" class="ResaInp"></input>
+                    <input type="date" name="ResaDate" id="Resa" class="ResaInp" ref={date}></input>
                     <FontAwesomeIcon icon={faCalendar} class='logo-Resa'></FontAwesomeIcon>
                     <div class="ligne3"></div>
                 </div>
                 <div>
                     <label for="SelectRoom" class="labelSelectRoom"></label>
-                    <select name="SelectRoom" id="SelectRoom" class="SelectRoomInp">
+                    <select name="SelectRoom" id="SelectRoom" class="SelectRoomInp" ref={salle}>
                         <option >Selectionnez une salle</option>
-                        <option value="">Majorelle</option>
-                        <option value="">Gruber</option>
-                        <option value="">Lamour</option>
-                        <option value="">Daume</option>
-                        <option value="">Baccarat</option>
+                        <option value="Majorelle">Majorelle</option>
+                        <option value="Gruber">Gruber</option>
+                        <option value="Lamour">Lamour</option>
+                        <option value="Daume">Daume</option>
+                        <option value="Baccarat">Baccarat</option>
                     </select>
                     <FontAwesomeIcon icon={faHotel} class='logo-SelectRoom'></FontAwesomeIcon>
                     <div class="ligne4"></div>
--- src/components/FormulaireInscriptions/SignInForm.js
+++ src/components/FormulaireInscriptions/SignInForm.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './SignInForm.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
@@ -7,25 +6,33 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import React, {useRef} from 'react';
 
 function SignInForm() {
-
+    const prenom = useRef();
+    const nom = useRef();
+    const email = useRef();
+    const password = useRef();
+    const telephone = useRef();
+    const ligue = useRef();
     const handleRegister = async (e) => {
         e.preventDefault();
-        const elements = Array.from(e.target.elements);
-        const body = {}
-        elements
-            .filter(item => item.type !== "submit")
-            .forEach(item => body[item.name] = item.value)
+        const body = {
+            prenom: prenom.current.value,
+            nom: nom.current.value,
+            email: email.current.value,
+            telephone: telephone.current.value,
+            password: password.current.value,
+            typeLigue: ligue.current.value
+        }
+        console.log(body);
         try {
            const res = await axios.post("http://localhost:8000/api/users",body)
            console.log(res);
         } catch (error) {
-            console.log(error.message);
+            console.log(error);
         }
     }
-
-
   return(
       <div>
         <form onSubmit={handleRegister}>
@@ -34,37 +41,45 @@ function SignInForm() {
                 
                 <div>
                     <label for="prenom" class="labelPrenom">PRENOM</label>
-                    <input type="text" name="prenom" id="prenom" class="prenomInp" required maxlength="35"></input>
+                    <input type="text" name="prenom" id="prenom" class="prenomInp" required maxlength="35" ref={prenom}></input>
                     <FontAwesomeIcon icon={faUser} class='logo-user1'></FontAwesomeIcon>
                     <div class="ligne1"></div>
                 </div>
                 <div>
                     <label for="nom" class="labelNom">NOM</label>
-                    <input type="text" name="nom" id="Nom" class="nomInp" required maxlength="35"></input>
+                    <input type="text" name="nom" id="Nom" class="nomInp" required maxlength="35" ref={nom}></input>
                     <FontAwesomeIcon icon={faUser} class='logo-user2'></FontAwesomeIcon>
                     <div class="ligne2"></div>
                 </div>
                 <div>
                     <label for="email" class="labelEmail">Email</label>
-                    <input type="text" name="email" id="email" class="emailInp" required maxlength="35"></input>
+                    <input type="text" name="email" id="email" class="emailInp" required maxlength="35" ref={email}></input>
                     <FontAwesomeIcon icon={faEnvelope} class='logo-mail'></FontAwesomeIcon>
                     <div class="ligne3"></div>
                 </div>
                 <div>
                     <label for="password" class="labelPassword">Password</label>
-                    <input type="password" name="password" id="password" class="passwordInp" required maxlength="35"></input>
+                    <input type="password" name="password" id="password" class="passwordInp" required maxlength="35" ref={password}></input>
                     <FontAwesomeIcon icon={faLock} class='logo-lock'></FontAwesomeIcon>
                     <div class="ligne4"></div>
                 </div>
                 <div>
                     <label for="telephone" class="labelTelephone">Telephone</label>
-                    <input type="text" name="telephone" id="telephone" class="telephoneInp" required maxlength="35"></input>
+                    <input type="text" name="telephone" id="telephone" class="telephoneInp" required maxlength="35" ref={telephone}></input>
                     <FontAwesomeIcon icon={faPhone} class="logo-phone"></FontAwesomeIcon>
                     <div class="ligne5"></div>
                 </div>
                 <div>
-                    <label for="Ligue" class="labelLigue">Ligue</label>
-                    <input type="text" name="typeLigue" id="ligue" class="ligueInp" required maxlength="35"></input>
+                    <label for="SelectLigue" class="labelSelectLigue"></label>
+                    <select name="SelectLigue" id="Ligue" class="SelectLigueInp" ref={ligue}>
+                        <option >Selectionnez une salle</option>
+                        <option value="1">Football</option>
+                        <option value="2">Tennis</option>
+                        <option value="3">Volley-ball</option>
+                        <option value="4">Basketball</option>
+                        <option value="5">Halterophilie</option>
+                        <option value="6">Rugby</option>
+                    </select>
                     <FontAwesomeIcon icon={faTrophy} class="logo-trophy"></FontAwesomeIcon>
                     <div class="ligne6"></div>
                 </div>
